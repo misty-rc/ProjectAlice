9d95115472e4d1e65aadc5d9da903ad0f7dc3cfb
misty_rc <misty.rc@gmail.com>
2013-01-10 17:53:57 +0900

    modify db manager

--- src/org/misty/rc/projectalice/BudgetManager.java
+++ src/org/misty/rc/projectalice/BudgetManager.java
@@ -1,8 +1,13 @@
 package org.misty.rc.projectalice;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,8 +20,36 @@ public class BudgetManager {
     private static final String DB_NAME = "budget_db";
     private static final int DB_VERSION = 1;
 
-    Context _context;
-    BudgetDBHelper _helper;
+    static final String T_BUDGET = "T_BUDGET";
+    static final String MST_CATEGORY = "MST_CATEGORY";
+
+    static final String COL_ID = "_id";
+    static final String COL_CATEGORY = "category";
+    static final String COL_DATE = "date";
+    static final String COL_MONEY = "money";
+    static final String COL_DELETE = "del_flg";
+
+    static final String COL_NAME = "category_name";
+
+    protected static final String DDL_BUDGET = new String(
+            "CREATE TABLE " + T_BUDGET + " ("
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COL_CATEGORY + " INTEGER NOT NULL, "
+            + COL_DATE + " INTEGER NOT NULL, "
+            + COL_MONEY + " INTEGER NOT NULL, "
+            + COL_DELETE + " INTEGER);"
+    );
+
+    protected static final String DDL_MST_CATEGORY = new String (
+            "CREATE TABLE " + MST_CATEGORY + " ("
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COL_NAME + " STRING NOT NULL, "
+            + COL_DELETE + " INTEGER);"
+    );
+
+    protected Context _context;
+    protected BudgetDBHelper _helper;
+    protected SQLiteDatabase _db;
 
     public BudgetManager(Context context) {
         this._context = context;
@@ -24,7 +57,25 @@ public class BudgetManager {
     }
 
     public void open() {
-        _helper.getReadableDatabase();
+        _db = _helper.getWritableDatabase();
+    }
+
+    public void close() {
+        _helper.close();
+    }
+
+    public void saveMoney(int money, int category) {
+        long now = System.currentTimeMillis();
+        ContentValues values = new ContentValues();
+        values.put(COL_CATEGORY, category);
+        values.put(COL_DATE, now);
+        values.put(COL_MONEY, money);
+        values.put(COL_DELETE, 0);
+        _db.insert(T_BUDGET, null, values);
+    }
+
+    public Cursor loadMoney() {
+        return _db.query(T_BUDGET,null,null,null,null,null,null);
     }
 
     private static class BudgetDBHelper extends SQLiteOpenHelper {
@@ -35,12 +86,23 @@ public class BudgetManager {
 
         @Override
         public void onConfigure(SQLiteDatabase db) {
+            //外部参照キー
             db.setForeignKeyConstraintsEnabled(true);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            //To change body of implemented methods use File | Settings | File Templates.
+            Log.d("ALICE", DDL_MST_CATEGORY);
+            Log.d("ALICE", DDL_BUDGET);
+
+            db.execSQL(DDL_MST_CATEGORY);
+            db.execSQL(DDL_BUDGET);
+
+            // test
+            String MST_1 = "insert into " + MST_CATEGORY + "(" + COL_NAME + "," + COL_DELETE + ")" + " values ('TEST CATE', 0)";
+            String MST_2 = "insert into " + MST_CATEGORY + "(" + COL_NAME + "," + COL_DELETE + ")" + " values ('ABCDEFG', 1)";
+            db.execSQL(MST_1);
+            db.execSQL(MST_2);
         }
 
         @Override
--- src/org/misty/rc/projectalice/MainActivity.java
+++ src/org/misty/rc/projectalice/MainActivity.java
@@ -2,13 +2,17 @@ package org.misty.rc.projectalice;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends FragmentActivity {
 	SectionPagerAdapter _sectionPagerAdapter;
@@ -39,6 +43,29 @@ public class MainActivity extends FragmentActivity {
 		_viewPager = (ViewPager) findViewById(R.id.pager);
 		_viewPager.setAdapter(_sectionPagerAdapter);
 		_viewPager.setCurrentItem(1);
+
+        //sqlite test
+        BudgetManager _budget = new BudgetManager(_context);
+        _budget.open();
+        Random random = new Random();
+        _budget.saveMoney(random.nextInt(), 1);
+        _budget.close();
+
+        // test
+        _budget.open();
+        Cursor c = _budget.loadMoney();
+        //startManagingCursor(c);
+        boolean isEof = c.moveToFirst();
+        while(isEof) {
+            int id = c.getInt(0); // id
+            int cat = c.getInt(1); // category
+            int date = c.getInt(2); // date
+            int money = c.getInt(3); // money
+            int del = c.getInt(4); // delete
+            isEof = c.moveToNext();
+            Log.d("ALICE", "cursor: " + id + ", " + cat + ", " + date + ", " + money + ", " + del);
+        }
+        _budget.close();
 	}
 	
 	void setOnClickListener(View view, int id) {
